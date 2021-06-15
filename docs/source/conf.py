5bf4a498ee7093bb9979ef7521d9b7a5b21bbe01
Brad Solomon <81818815+brsolomon-deloitte@users.noreply.github.com>
2021-06-15 11:39:33 -0400

    Version 0.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,10 +22,10 @@ copyright = "2021, Deloitte Consulting LLP"
 author = "Deloitte Consulting LLP"
 
 # The short X.Y version
-version = "0.0.1"
+version = "0.1.0"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
